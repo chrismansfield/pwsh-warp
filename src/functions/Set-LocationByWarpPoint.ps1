fa599d3d4e065bfb706151d5302ab646fae246da
Christoffer Mansfield <christoffer.mansfield@gmail.com>
2019-05-11 17:32:46 +0200

    Warp to provided point

--- src/functions/Set-LocationByWarpPoint.ps1
+++ src/functions/Set-LocationByWarpPoint.ps1
@@ -3,7 +3,7 @@ function Set-LocationByWarpPoint{
 	Param(
 		[Parameter(Position=0)]$Name
 	)	
-	$targetLocation = Get-WarpTable | Select -ExpandProperty pw
+	$targetLocation = Get-WarpTable | Select -ExpandProperty $Name
 	if($targetLocation -eq $null){
 		Write-Error "No warp point named $Name. Use Add-WarpPoint (wd add) to add a warp point"
 	} else {
